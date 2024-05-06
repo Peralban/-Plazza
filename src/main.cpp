bdbf285f631b45f07d625bc0e4aac605cf978184
PerAlban <alban.peralta@epitech.eu>
2024-05-06 16:24:13 +0200

    :sparkles: Create Temporary main

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2024
+** main.cpp
+** File description:
+** DESCRIPTION
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
