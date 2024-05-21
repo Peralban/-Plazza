98bf73e7a0a28567e7c9dccfaced4de5df747dac
Thomaltarix <thomas.boue@epitech.eu>
2024-05-21 15:12:46 +0200

    :sparkles: Add the command-line error handling and storing

---
+++ src/Arguments.cpp
@@ -0,0 +1,51 @@
+/*
+** EPITECH PROJECT, 2024
+** Plazza
+** File description:
+** Arguments
+*/
+
+#include "Arguments.hpp"
+
+Plazza::Arguments::Arguments(int ac, char **av)
+{
+    if (ac != 4) {
+        displayUsage();
+        throw Errors::ArgumentNumber("Invalid number of arguments");
+    } if (std::stof(av[1]) < 0 || std::stof(av[1]) > 1) {
+        displayUsage();
+        throw Errors::WrongMultiplier("Invalid multiplier");
+    } if (std::stoi(av[2]) <= 0) {
+        displayUsage();
+        throw Errors::WrongCookNumber("Invalid number of cooks");
+    } if (std::stof(av[3]) <= 0) {
+        displayUsage();
+        throw Errors::WrongRestockTime("Invalid restock time");
+    }
+    _multiplier = std::stof(av[1]);
+    cookNumber = std::stoi(av[2]);
+    restockTime = std::stof(av[3]);
+}
+
+double Plazza::Arguments::getMultiplier() const
+{
+    return _multiplier;
+}
+
+int Plazza::Arguments::getCookNumber() const
+{
+    return cookNumber;
+}
+
+double Plazza::Arguments::getRestockTime() const
+{
+    return restockTime;
+}
+
+void Plazza::Arguments::displayUsage()
+{
+    std::cout << "USAGE: ./plazza multiplier cooks restock_time" << std::endl;
+    std::cout << "\tmultiplier: positive float" << std::endl;
+    std::cout << "\tcooks: positive integer" << std::endl;
+    std::cout << "\trestock_time: positive float" << std::endl;
+}
