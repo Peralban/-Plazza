72bd4c39177314b9a6560e1bd1be808482b72aa3
landryarki <landry.gigant@epitech.eu>
2024-05-24 15:02:56 +0200

    :recycle: update message queue IPC

--- src/messageQueue/messageQueueIPC.hpp
+++ src/messageQueue/messageQueueIPC.hpp
@@ -28,9 +28,9 @@ public:
      * @param id The ID of the message queue.
      * Initializes the key and message queue ID.
      */
-    explicit MessageQueueIPC(int id) : _key(0), _msg_id(0) {
-        std::string tmpfile = "/tmp/MessageQueueIPC_Plazza";
-        std::ofstream file(tmpfile, std::ios::out | std::ios::trunc);
+    explicit MessageQueueIPC(int id) : _key(0), _msg_id(0), _id(id) {
+        std::string tmpfile = "/tmp/MessageQueueIPC_Plazza" + std::to_string(id);
+        std::ofstream file(tmpfile);
         if (!file.is_open()) {
             throw std::runtime_error("Cannot create tmpfile");
         }
@@ -42,15 +42,16 @@ public:
         _msg_id = msgget(_key, 0666 | IPC_CREAT);
     }
 
+    MessageQueueIPC() = delete;
+
+    ~MessageQueueIPC() = default;
+
     /**
-     * @brief Destructor for the MessageQueueIPC class.
-     * Removes the message queue.
+     * @brief Removes the message queue.
      */
-    ~MessageQueueIPC() {
+    void destroy() {
         msgctl(_msg_id, IPC_RMID, nullptr);
     }
-
-    MessageQueueIPC() = delete;
 
     /**
      * @brief Pushes a message to the message queue.
@@ -62,7 +63,7 @@ public:
         std::string message_str = message;
         strcpy(buf.mtext, message_str.c_str());
         buf.mtype = 1;
-        msgsnd(_msg_id, &buf, sizeof(buf.mtext), 0);
+        msgsnd(_msg_id, &buf, sizeof(buf.mtext), IPC_NOWAIT);
     }
 
     /**
@@ -72,7 +73,18 @@ public:
     T pop()
     {
         message_buf buf = {};
-        msgrcv(_msg_id, &buf, sizeof(buf.mtext), 1, IPC_NOWAIT);
+        msgrcv(_msg_id, &buf, sizeof(buf.mtext), 0, IPC_NOWAIT);
+        return T(buf.mtext);
+    }
+
+    /**
+     * @brief Pops a message from the message queue at index.
+     * @return The popped message.
+     */
+    T pop(int index)
+    {
+        message_buf buf = {};
+        msgrcv(_msg_id, &buf, sizeof(buf.mtext), index, IPC_NOWAIT);
         return T(buf.mtext);
     }
 
@@ -88,6 +100,17 @@ public:
     }
 
     /**
+     * @brief Gets the message at the specified index without removing it.
+     * @return The message at the front of the queue.
+     */
+    [[nodiscard]] T operator[](int index)
+    {
+        message_buf buf = {};
+        msgrcv(_msg_id, &buf, sizeof(buf.mtext), index, MSG_COPY | IPC_NOWAIT);
+        return T(buf.mtext);
+    }
+
+    /**
      * @brief Checks if the message queue is empty.
      * @return True if the queue is empty, false otherwise.
      */
@@ -98,9 +121,35 @@ public:
         return buf.mtext[0] == 0;
     }
 
+    /**
+     * @brief Resets the message queue.
+     */
+    void reset()
+    {
+        msgctl(_msg_id, IPC_RMID, nullptr);
+        _msg_id = msgget(_key, 0666 | IPC_CREAT);
+    }
+
+    /**
+     * @brief Gets the size of the message queue.
+     * @return The size of the message queue.
+     */
+    int size()
+    {
+        struct msqid_ds buf = {};
+        msgctl(_msg_id, IPC_STAT, &buf);
+        return (int)buf.msg_qnum;
+    }
+
+    [[ nodiscard ]] int getID() const
+    {
+        return _id;
+    }
+
 private:
     key_t _key; ///< The key used for the message queue.
-    int _msg_id; ///< The ID of the message queue.
+    int _msg_id; ///< The ID of the message queue used for communication.
+    int _id; ///< The ID of the message queue.
 
     /**
      * @struct message_buf
